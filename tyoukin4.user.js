8ca399aa37bfb8a2187b9aacb91616d17f072db2
midoriI2026 <106957602+midoriI2026@users.noreply.github.com>
2026-04-29 16:27:01 +0900

    Add user script for pre-approval check

---
+++ tyoukin4.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         ★超勤：承認前チェック（同一タブ完成版）
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBTaskForm&did=150*
+// @match        https://midorinet-iwate.cybozu.com/o/ag.cgi?page=DBForm&did=150*
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/midoriI2026/tyoukin4/main/tyoukin4.js?v=1.0
+// @grant        none
+// ==/UserScript==
